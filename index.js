bb2613878fb0052a25009278879544489759bbbe
Mustakim-Khan <mustakimzidan@gmail.com>
2022-12-12 15:11:31 +0100

    chatBot implementaion without appointments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -12,6 +13,27 @@ const io = new Server(server, {
 let vehiculeInfo = {};
 
 const users = [];
+
+// Fake data
+const contact = {
+  contactEmail: "contact@gmail.com",
+  contactNumber: "0160293759",
+};
+
+const maintenanceAppointments = [
+  {
+    id: 1,
+    date: new Date("December 13, 2022 15:00:00"),
+  },
+  {
+    id: 2,
+    date: new Date("December 15, 2022 10:00:00"),
+  },
+  {
+    id: 3,
+    date: new Date("December 16, 2022 13:00:00"),
+  },
+];
 
 const startChatBot = (socket) => {
   socket.emit("ask_help_type", {
@@ -66,9 +88,29 @@ io.on("connection", (socket) => {
 
   socket.on("send_help_type", (res) => {
     if (res === 1) {
-      socket.emit("ask_vehicule_year", {
+      // socket.emit("ask_vehicule_year", {
+      //   from: "server",
+      //   txt: "Saisir l'année d'i mmatriculation",
+      // });
+      sendMessage(socket, "ask_vehicule_year", {
         from: "server",
         txt: "Saisir l'année d'immatriculation",
+      });
+    } else if (res === 2) {
+      console.log("bla");
+      sendMessage(socket, "ask_usage_type", {
+        from: "server",
+        txt: `Type d'usage ?
+1 : Routier
+2 : Tout terrain
+3 : Sportif`,
+      });
+    } else {
+      sendMessage(socket, "ask_contact_type", {
+        from: "server",
+        txt: `Type de contact ?
+1 : Email
+2 : Téléphone`,
       });
     }
   });
@@ -78,7 +120,12 @@ io.on("connection", (socket) => {
       ...vehiculeInfo,
       vehiculeYear: res,
     };
-    socket.emit("ask_last_maintenance_date", {
+    // socket.emit("ask_last_maintenance_date", {
+    //   from: "server",
+    //   txt: "Saisir la date du dernier entretien",
+    //   vehiculeInfo,
+    // });
+    sendMessage(socket, "ask_last_maintenance_date", {
       from: "server",
       txt: "Saisir la date du dernier entretien",
       vehiculeInfo,
@@ -98,17 +145,17 @@ io.on("connection", (socket) => {
     if (yearDiff > 1) {
       // TODO  choix date de rdv avec calendrier
     } else {
-      socket.emit("ask_km_since_last_maintenance", {
+      sendMessage(socket, "ask_km_since_last_maintenance", {
         from: "server",
         txt: "Saisir le nombre de kilomètres parcourus depuis le dernier entretien",
         vehiculeInfo,
       });
+      // socket.emit("ask_km_since_last_maintenance", {
+      //   from: "server",
+      //   txt: "Saisir le nombre de kilomètres parcourus depuis le dernier entretien",
+      //   vehiculeInfo,
+      // });
     }
-    // socket.emit("ask_last_maintenance_date", {
-    //   from: "server",
-    //   txt: "Saisir la date du dernier entretien",
-    //   vehiculeInfo,
-    // });
   });
 
   socket.on("send_km_since_maintenance_date", (res) => {
@@ -121,13 +168,20 @@ io.on("connection", (socket) => {
       console.log("TOOO : demande rdv");
       // TODO rdv
     } else {
-      socket.emit("ask_do_revision", {
+      sendMessage(socket, "ask_do_revision", {
         from: "server",
         txt: `Souhaitez-vous effectuer une révision ?
 1 : Oui
 2 : Non`,
         vehiculeInfo,
       });
+      //       socket.emit("ask_do_revision", {
+      //         from: "server",
+      //         txt: `Souhaitez-vous effectuer une révision ?
+      // 1 : Oui
+      // 2 : Non`,
+      //         vehiculeInfo,
+      //       });
     }
   });
 
@@ -148,6 +202,35 @@ io.on("connection", (socket) => {
   //     txt: msg,
   //   });
   // });
+
+  socket.on("send_usage_type", (res) => {
+    console.log(res);
+    if (res === 1) {
+      // TODO demande rdv routier
+      socket.emit("reset_chat");
+    } else if (res === 2) {
+      // TODO demande rdv tout-terrain
+      socket.emit("reset_chat");
+    } else {
+      // TODO demande rdv sportif
+      socket.emit("reset_chat");
+    }
+  });
+
+  socket.on("send_contact_type", (res) => {
+    console.log(maintenanceAppointments);
+    if (res === 1) {
+      sendMessage(socket, "send_contact_email", {
+        from: "server",
+        txt: `Email : ${contact.contactEmail}`,
+      });
+    } else {
+      sendMessage(socket, "send_contact_number", {
+        from: "server",
+        txt: `Number : ${contact.contactNumber}`,
+      });
+    }
+  });
 });
 
 server.listen(3000, () => {
